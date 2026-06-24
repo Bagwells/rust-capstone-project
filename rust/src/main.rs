6c3de9f1d2356c887af822a7ac1c986e2b4d1a9a
bagwells <timmyayomidebagwells@outlook.com>
2026-06-24 19:30:43 +0100

    Bitcoin core rpc connection with username and password

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -9,8 +9,8 @@ use std::io::Write;
 
 // Node access params
 const RPC_URL: &str = "http://127.0.0.1:18443"; // Default regtest RPC port
-const RPC_USER: &str = "alice";
-const RPC_PASS: &str = "password";
+const RPC_USER: &str = "bagwellsrpc";
+const RPC_PASS: &str = "bitcoinrpc";
 
 // You can use calls not provided in RPC lib API using the generic `call` function.
 // An example of using the `send` RPC call, which doesn't have exposed API.
