0d77a702b1839ee46f9323cfe7d3532d476e7bc0
bagwells <timmyayomidebagwells@outlook.com>
2026-07-03 05:47:00 +0100

    ...submission

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -9,7 +9,7 @@ use std::io::Write;
 
 // Node access params
 const RPC_URL: &str = "http://127.0.0.1:18443"; // Default regtest RPC port
-const RPC_USER: &str = "Alice";
+const RPC_USER: &str = "alice";
 const RPC_PASS: &str = "password";
 
 // You can use calls not provided in RPC lib API using the generic `call` function.
@@ -17,8 +17,14 @@ const RPC_PASS: &str = "password";
 // You can also use serde_json `Deserialize` derivation to capture the returned json result.
 
 #[derive(Deserialize)]
+#[serde(untagged)]
+enum AddressField {
+    One(String),
+    Many(Vec<String>),
+}
+#[derive(Deserialize)]
 struct ScriptPubKey {
-    address: Option<String>,
+    address: Option<AddressField>,
     addresses: Option<Vec<String>>,
 }
 
@@ -31,8 +37,10 @@ struct VinPrevout {
 }
 
 #[derive(Deserialize)]
-#[serde(rename_all = "camelCase")] 
+#[serde(rename_all = "camelCase")]
 struct Vin {
+    txid: Option<String>,
+    vout: Option<u32>,
     prevout: Option<VinPrevout>,
 }
 
@@ -56,11 +64,26 @@ struct VerboseTransaction {
     decoded: DecodedTx,
 }
 
-fn address_from_spk(spk: &ScriptPubKey) -> String {
-    spk.address
-        .clone()
-        .or_else(|| spk.addresses.as_ref().and_then(|a: &Vec<String>| a.first().cloned()))
+fn address_from_scriptpubkey(spk: &ScriptPubKey) -> String {
+    // try "address" field first (string or array)
+    if let Some(field) = &spk.address {
+        return match field {
+            AddressField::One(s) => s.clone(),
+            AddressField::Many(v) => v
+                .first()
+                .expect("address array was empty")
+                .clone(),
+        };
+    }
+    // fallback to legacy "addresses" array
+    spk.addresses
+        .as_ref()
+        .and_then(|a| a.first().cloned())
         .expect("address missing from scriptPubKey")
+}
+
+fn scriptpubkey_from_value(v: &serde_json::Value) -> ScriptPubKey {
+    serde_json::from_value(v["scriptPubKey"].clone()).expect("scriptPubKey parse failed")
 }
 
 fn send(rpc: &Client, addr: &str) -> bitcoincore_rpc::Result<String> {
@@ -82,8 +105,6 @@ fn send(rpc: &Client, addr: &str) -> bitcoincore_rpc::Result<String> {
     Ok(send_result.txid)
 }
 
-
-
 fn main() -> bitcoincore_rpc::Result<()> {
     // Connect to Bitcoin Core RPC
     let rpc = Client::new(
@@ -97,13 +118,13 @@ fn main() -> bitcoincore_rpc::Result<()> {
     )?;
 
     let trader_rpc = Client::new(
-        &format!("{}/wallet/{}", RPC_URL, "Trader"), 
+        &format!("{}/wallet/{}", RPC_URL, "Trader"),
         Auth::UserPass(RPC_USER.to_owned(), RPC_PASS.to_owned()),
     )?;
 
     // Get blockchain info
-    let blockchain_info = rpc.get_blockchain_info()?;
-    println!("Blockchain Info: {:?}", blockchain_info);
+    let blockchain_info: serde_json::Value = rpc.call("getblockchaininfo", &[])?;
+    println!("Blockchain Info: {}", blockchain_info.to_string());
 
     // Create/Load the wallets, named 'Miner' and 'Trader'. Have logic to optionally create/load them if they do not exist or not loaded already.
     let wallet_names = vec!["Miner", "Trader"];
@@ -111,30 +132,27 @@ fn main() -> bitcoincore_rpc::Result<()> {
 
     for wallet in wallet_names {
         if loaded_wallets.contains(&wallet.to_string()) {
-            rpc.load_wallet(&wallet.to_string())?;
-            println!("{wallet} is loaded");
+            println!("{wallet} is already loaded");
         } else {
             if rpc.load_wallet(wallet).is_err() {
-                rpc.create_wallet(
-                    &wallet.to_string(),
-                    None,
-                    None,
-                    None,
-                    None
-            )?;
+                rpc.create_wallet(&wallet.to_string(), None, None, None, None)?;
             }
-            println!("{wallet} is created and loaded");
+            println!("{wallet} is loaded");
         }
     }
 
     // Generate spendable balances in the Miner wallet. How many blocks needs to be mined?
-    let miner_address = miner_rpc.get_new_address(Some("Mining Reward"), None)?.assume_checked();
-    rpc.generate_to_address(101, &miner_address)?;  // 101 blocks is generated to because of the coinbase maturity consesus rule of the coinbase transaction that cannot be spent until the minimum output of 100 is confirmed
+    let miner_address = miner_rpc
+        .get_new_address(Some("Mining Reward"), None)?
+        .assume_checked();
+    rpc.generate_to_address(10, &miner_address)?; // 101 blocks is generated to because of the coinbase maturity consesus rule of the coinbase transaction that cannot be spent until the minimum output of 100 is confirmed
     let miner_balance = miner_rpc.get_balance(None, None)?;
-    println!("Miner Balance: {}", miner_balance); 
+    println!("Miner Balance: {}", miner_balance);
 
     // Load Trader wallet and generate a new address
-    let trader_address = trader_rpc.get_new_address(Some("Received"), None)?.assume_checked();
+    let trader_address = trader_rpc
+        .get_new_address(Some("Received"), None)?
+        .assume_checked();
     println!("Trader Address: {}", trader_address);
 
     // Send 20 BTC from Miner to Trader
@@ -142,7 +160,7 @@ fn main() -> bitcoincore_rpc::Result<()> {
     let amount_to_send = Amount::from_btc(btc).unwrap();
     let mut txid;
     if miner_balance > amount_to_send {
-        let result = rpc.send_to_address(
+        let result = miner_rpc.send_to_address(
             &trader_address,
             amount_to_send,
             None,
@@ -164,42 +182,53 @@ fn main() -> bitcoincore_rpc::Result<()> {
     println!("Mempool Info: {:?}", mempool_info);
 
     // Mine 1 block to confirm the transaction
-    rpc.generate_to_address(1, &miner_address)?;
+    rpc.generate_to_address(101, &miner_address)?;
 
     // Extract all required transaction details
     let tx_details = miner_rpc.call::<VerboseTransaction>(
         "gettransaction",
-        &[json!(txid), json!(null), json!(true)],
+        &[json!(txid.to_string()), json!(null), json!(true)],
     )?;
     let vin = &tx_details.decoded.vin[0];
-    let prevout = vin.prevout.as_ref().expect("vin prevout missing");
-    let miner_input_address = address_from_spk(
-        prevout.script_pub_key.as_ref().expect("prevout scriptPubKey missing"),
-    );
-    let miner_input_amount = prevout.value.expect("prevout value missing");
-    let trader_vout = &tx_details.decoded.vout[0];
-    let trader_input_address = address_from_spk(&trader_vout.script_pub_key);
+    let prev_txid = vin.txid.as_ref().expect("vin txid missing");
+    let prev_vout = vin.vout.expect("vin vout missing") as usize;
+    let prev: serde_json::Value =
+        miner_rpc.call("gettransaction", &[json!(prev_txid), json!(null), json!(true)])?;
+    let prev_output = &prev["decoded"]["vout"][prev_vout];
+    let miner_input_address = address_from_scriptpubkey(&scriptpubkey_from_value(prev_output));
+    let miner_input_amount = prev_output["value"].as_f64().unwrap();
+    let trader_str = trader_address.to_string();
+    let trader_vout = tx_details
+        .decoded
+        .vout
+        .iter()
+        .find(|v| address_from_scriptpubkey(&v.script_pub_key) == trader_str)
+        .expect("trader output not found");
+    let change_vout = tx_details
+        .decoded
+        .vout
+        .iter()
+        .find(|v| address_from_scriptpubkey(&v.script_pub_key) != trader_str)
+        .expect("change output not found");
+    let trader_input_address = address_from_scriptpubkey(&trader_vout.script_pub_key);
     let trader_input_amount = trader_vout.value;
-    let change_vout = &tx_details.decoded.vout[1];
-    let miner_change_address = address_from_spk(&change_vout.script_pub_key);
+    let miner_change_address = address_from_scriptpubkey(&change_vout.script_pub_key);
     let miner_change_amount = change_vout.value;
     let fee = tx_details.fee.expect("fee missing").abs();
     let block_height = tx_details.blockheight.expect("blockheight missing");
     let block_hash = tx_details.blockhash.expect("blockhash missing");
-
-
-    // Write the data to ../out.txt in the specified format given in readme.md
+    // Write the data to ../out.txt (10 raw lines, no labels)
     let mut file = File::create("../out.txt")?;
-    file.write_all(format!("txid: {}\n", txid).as_bytes())?;
-    file.write_all(format!("miner_input_address: {}\n", miner_input_address).as_bytes())?;
-    file.write_all(format!("miner_input_amount: {}\n", miner_input_amount).as_bytes())?;
-    file.write_all(format!("trader_input_address: {}\n", trader_input_address).as_bytes())?;
-    file.write_all(format!("trader_input_amount: {}\n", trader_input_amount).as_bytes())?;
-    file.write_all(format!("miner_change_address: {}\n", miner_change_address).as_bytes())?;
-    file.write_all(format!("miner_change_amount: {}\n", miner_change_amount).as_bytes())?;
-    file.write_all(format!("fee: {}\n", fee).as_bytes())?;
-    file.write_all(format!("block_height: {}\n", block_height).as_bytes())?;
-    file.write_all(format!("block_hash: {}\n", block_hash).as_bytes())?;
+    writeln!(file, "{}", txid)?;
+    writeln!(file, "{}", miner_input_address)?;
+    writeln!(file, "{}", miner_input_amount)?;
+    writeln!(file, "{}", trader_input_address)?;
+    writeln!(file, "{}", trader_input_amount)?;
+    writeln!(file, "{}", miner_change_address)?;
+    writeln!(file, "{}", miner_change_amount)?;
+    writeln!(file, "{}", fee)?;
+    writeln!(file, "{}", block_height)?;
+    writeln!(file, "{}", block_hash)?;
     println!("Data written to ../out.txt");
 
     Ok(())
